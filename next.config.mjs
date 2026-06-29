66c0d89802b47cb5d3a3418493b15a00b822206a
Tu Pham <=>
2026-06-29 14:03:19 +0700

    Integrate github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+const repoName = "Ptit-japanese-training";
 const nextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
 };
 export default nextConfig;
